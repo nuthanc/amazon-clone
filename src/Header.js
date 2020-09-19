be2d7909ee8a5fbdb9610f9d1e4be5bfdc856266
Nuthan Chandra <nuthanc@juniper.net>
2020-09-19 16:52:38 +0530

    Add Amazon logo with proper css

--- src/Header.js
+++ src/Header.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import './Header.css';
+import { Link } from 'react-router-dom';
 
 function Header() {
   return (
     <nav className="header">
       {/* logo on the left */}
-      <img
-        className="header__logo"
-        src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
-        alt=""
-      />
+      <Link to="/">
+        <img
+          className="header__logo"
+          src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+          alt=""
+        />
+      </Link>
+
       {/* search box */}
       {/* 3 links */}
       {/* Basket Icon with number */}
