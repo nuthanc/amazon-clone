0e8bd3a736261414f4c941916b555ce1c439650b
Nuthan Chandra <nuthanc@juniper.net>
2020-09-19 16:26:07 +0530

    Basic setup and Header component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Header from './Header';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/checkout">
+          <h1>This is the checkout page</h1>
+        </Route>
+        <Route path="/login">
+          <h1>This is the login page</h1>
+        </Route>
+        <Route path="/">
+          <div className="app">
+            <Header />
+            <h1>Let's build an Amazon CLONE WITH React JS</h1>
+          </div>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+  return (
+    <nav className="header">
+      {/* logo on the left */}
+      <img
+        className="header__logo"
+        src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+        alt=""
+      />
+      {/* search box */}
+      {/* 3 links */}
+      {/* Basket Icon with number */}
+    </nav>
+  );
+}
+
+export default Header;
