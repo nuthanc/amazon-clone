5d9a1ea48f53b1a0b53dba390707d76ba60db719
Nuthan Chandra <nuthanc@juniper.net>
2020-10-09 20:15:07 +0530

    Finish banner

---
+++ src/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Home.css'
+
+const Home = () => {
+  return (
+    <div className="home">
+      <img
+        className="home__image"
+        src="https://images-eu.ssl-images-amazon.com/images/G/31/AmazonVideo/2020/X-site/ServiceMktg/1500x600_Hero-Tall_np._CB403152874_.jpg"
+        alt=""
+      />
+    </div>
+  );
+};
+
+export default Home;
